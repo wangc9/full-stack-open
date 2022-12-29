5715cb24800a6c33336729e5da43ab38ad3e9b03
Chen Wang <99030776+wangc9@users.noreply.github.com>
2022-12-29 13:51:33 +0200

    Add exercise 2.6 - 2.10

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Persons from './components/Persons'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
+  const [filter, setFilter] = useState('')
+  const [filterPersons, setFilterPersons] = useState(persons)
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value)
+    setFilterPersons(persons.filter((person) =>
+      (person.name.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1)))
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter onChange={handleFilterChange} value={filter} />
+      <h2>add a new</h2>
+      <PersonForm persons={persons} setPersons={setPersons} />
+      <h2>Numbers</h2>
+      <table>
+        <tbody>
+          {filter === '' ?
+            <Persons filterPerson={persons} />
+            :
+            <Persons filterPerson={filterPersons} />
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+export default App
