bcbb500d376c33e45d9cd7f2c6983465df8f0e88
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-05-12 23:15:14 +0300

    Finish exercise 3.3

--- phonebook/index.js
+++ phonebook/index.js
@@ -30,6 +30,12 @@ app.get("/api/persons", (request, response) => {
     response.json(persons);
 });
 
+app.get("/api/person/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((entry) => entry.id === id)
+    person ? response.json(person) : response.status(404).end();
+});
+
 app.get("/info", (request, response) => {
     const total = persons.reduce((count, people) => {
         if (people.id) count ++;
