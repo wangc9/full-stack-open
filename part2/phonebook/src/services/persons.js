6f6010915ea08f00497bd1644d9fba7f9ad72f71
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-05-14 00:27:42 +0300

    Finish Exercise 3.9. Push backend to fly.io

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "https://fso-charles-phonebook.fly.dev/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
@@ -12,12 +12,12 @@ const create = (newObject) => {
 };
 
 const remove = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
+  const request = axios.delete(`https://fso-charles-phonebook.fly.dev/api/person/${id}`);
   return request.then((response) => response);
 };
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  const request = axios.put(`https://fso-charles-phonebook.fly.dev/api/person/${id}`, newObject);
   return request.then((response) => response.data);
 };
 
