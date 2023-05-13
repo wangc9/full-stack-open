9efbe5051040b6bf06af461717ed39ef0fc8b03c
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-05-13 23:13:58 +0300

    Finish exercise 3.8

--- phonebook/index.js
+++ phonebook/index.js
@@ -3,7 +3,10 @@ const morgan = require("morgan");
 const app = express()
 
 app.use(express.json());
-app.use(morgan("tiny"));
+morgan.token("entry", (request, response) => {
+    return request.method === "POST" ? JSON.stringify(request.body) : " "
+});
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :entry"));
 
 let persons = [
     {
@@ -61,7 +64,7 @@ app.post("/api/persons", (request, response) => {
             }
         }
         newPerson.id = id;
-        console.log(`new person entry: ${newPerson.name}, ${newPerson.number}`);
+        // console.log(`new person entry: ${newPerson.name}, ${newPerson.number}`);
         persons = persons.concat(newPerson);
         response.json(newPerson);
     }
