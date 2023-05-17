2d58eb68a4c83d53378ea4eee249c275ee84e68e
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-05-17 21:57:23 +0300

    Finish Exercise 3.20. Support number check.

--- phonebook/models/person.js
+++ phonebook/models/person.js
@@ -20,6 +20,13 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d{5,}$/.test(v);
+                },
+            message: props => `${props.value} is not a valid phone number!`
+        },
         required: true
     }
 });
