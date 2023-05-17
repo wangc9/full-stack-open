d68c001ac22aadcb29279527dc451d1fe323b849
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-05-17 18:13:16 +0300

    Finish Exercise 3.18. Support info and id check.

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -12,12 +12,12 @@ const create = (newObject) => {
 };
 
 const remove = (id) => {
-  const request = axios.delete(`/api/person/${id}`);
+  const request = axios.delete(`${baseUrl}/${id}`);
   return request.then((response) => response);
 };
 
 const update = (id, newObject) => {
-  const request = axios.put(`/api/person/${id}`, newObject);
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
   return request.then((response) => response.data);
 };
 
