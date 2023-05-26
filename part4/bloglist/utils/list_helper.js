9d12bad8b6e8a86bc61ef26902b2c41ed1a2a796
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-05-27 01:20:44 +0300

    Finish exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,4 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1;
 
-module.exports = { dummy };
+const totalLikes = (blogs) => (blogs.length === 0
+  ? 0 : blogs.reduce((sum, post) => sum + post.likes, 0));
+
+module.exports = { dummy, totalLikes };
