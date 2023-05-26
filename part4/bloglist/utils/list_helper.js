9ee6eeab0f7d0d9ee71228b2d89725a728911b3b
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-05-27 01:41:40 +0300

    Finish exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,4 +4,23 @@ const dummy = (blogs) => 1;
 const totalLikes = (blogs) => (blogs.length === 0
   ? 0 : blogs.reduce((sum, post) => sum + post.likes, 0));
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  let max = blogs[0];
+  for (let i = 1; i < blogs.length; i++) {
+    if (blogs[i].likes > max.likes) {
+      max = blogs[i];
+    }
+  }
+  const result = {
+    title: max.title,
+    author: max.author,
+    likes: max.likes,
+  };
+
+  return result;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
