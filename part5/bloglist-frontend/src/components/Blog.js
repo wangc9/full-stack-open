63abaaa34144cf3e4fa8dcd73e370947c6811c46
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-06-17 12:16:16 +0300

    Finish exercise 5.7 Show details

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,38 @@
+import { useState } from 'react';
+
 function Blog({ blog }) {
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState('view');
+  const showWhenVisible = { display: show ? '' : 'none' };
+
+  const handleClick = () => {
+    setShow(!show);
+    setName(name === 'view' ? 'hide' : 'view');
+  };
+
   return (
-    <div>
-      {blog.title}
-      {' '}
-      {blog.author}
+    <div className="blog">
+      <div>
+        {blog.title}
+        {' '}
+        {blog.author}
+        {' '}
+        <button onClick={handleClick}>{name}</button>
+        <div style={showWhenVisible}>
+          <div>
+            {blog.url}
+          </div>
+          <div>
+            likes
+            {' '}
+            {blog.likes}
+            <button>like</button>
+          </div>
+          <div>
+            {blog.user.name}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
