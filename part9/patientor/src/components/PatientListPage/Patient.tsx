544de71a9af7d46f836024ed46c01a16e2c1a2c9
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-06-30 00:54:59 +0300

    Finish exercise 9.25

--- part9/patientor/src/components/PatientListPage/Patient.tsx
+++ part9/patientor/src/components/PatientListPage/Patient.tsx
@@ -1,9 +1,52 @@
-import {Diagnosis, Patient} from "../../types";
+import {Diagnosis, Entry, HealthCheckRating, Patient} from "../../types";
 import diagnoseService from '../../services/diagnoses';
 import WomanIcon from '@mui/icons-material/Woman';
 import ManIcon from '@mui/icons-material/Man';
 import TransgenderIcon from '@mui/icons-material/Transgender';
-import {useEffect, useState} from "react";
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import WorkIcon from '@mui/icons-material/Work';
+import ErrorIcon from '@mui/icons-material/Error';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import React, {useEffect, useState} from "react";
+
+const healthData = (health: HealthCheckRating) => {
+  switch (health) {
+    case HealthCheckRating.LowRisk:
+      return (
+        <div>
+          <ErrorIcon />
+        </div>
+      );
+    case HealthCheckRating.HighRisk:
+      return (
+        <div>
+          <ErrorIcon /> <ErrorIcon />
+        </div>
+      );
+    case HealthCheckRating.CriticalRisk:
+      return (
+        <div>
+          <ErrorIcon /> <ErrorIcon /> <ErrorIcon />
+        </div>
+      );
+    case HealthCheckRating.Healthy:
+      return (
+        <div>
+          <FavoriteIcon />
+        </div>
+      );
+    default:
+      return (
+        <div>
+          <QuestionMarkIcon />
+        </div>
+      );
+  }
+};
+
+
 
 export const PatientDetail = ({patient}: {patient: Patient}) => {
   const [name, setName] = useState<string[]>([]);
@@ -21,6 +64,83 @@ export const PatientDetail = ({patient}: {patient: Patient}) => {
     getDiagnoses().then(res => setCodes(res));
   }, []);
 
+  const EntryDetails: React.FC<{entry: Entry}> = ({entry}) => {
+    switch (entry.type) {
+      case "HealthCheck":
+        return (
+          <div>
+            {entry.date} <HealthAndSafetyIcon />
+            <br />
+            {entry.description}
+            <br />
+            {healthData(entry.healthCheckRating)}
+            <br />
+            diagnosed by {entry.specialist}
+            <br />
+            {entry.diagnosisCodes && (
+              <ul>
+                {entry.diagnosisCodes.map((code) => {
+                  const name = codes.find((d) => d.code === code);
+                  return (<li key={code}>{code} {name?.name}</li>)
+                })}
+              </ul>
+            )}
+          </div>
+        );
+      case "Hospital":
+        return (
+          <div>
+            {entry.date} <LocalHospitalIcon />
+            <br />
+            {entry.description}
+            <br />
+            diagnosed by {entry.specialist}
+            <br />
+            {entry.diagnosisCodes && (
+              <ul>
+                {entry.diagnosisCodes.map((code) => {
+                  const name = codes.find((d) => d.code === code);
+                  return (<li key={code}>{code} {name?.name}</li>)
+                })}
+              </ul>
+            )}
+            <br />
+            discharge: {entry.discharge.date} - {entry.discharge.criteria}
+          </div>
+        );
+      case "OccupationalHealthcare":
+        return (
+          <div>
+            {entry.date} <WorkIcon /> {entry.employerName}
+            <br />
+            {entry.description}
+            <br />
+            diagnosed by {entry.specialist}
+            <br />
+            {entry.diagnosisCodes && (
+              <ul>
+                {entry.diagnosisCodes.map((code) => {
+                  const name = codes.find((d) => d.code === code);
+                  return (<li key={code}>{code} {name?.name}</li>)
+                })}
+              </ul>
+            )}
+            <br />
+            {entry.sickLeave && (
+              <div>
+                Sick leave: {entry.sickLeave.startDate} - {entry.sickLeave.endDate}
+              </div>
+            )}
+          </div>
+        );
+      default:
+        return (
+          <div>
+            No data available
+          </div>
+        );
+    }
+  }
 
   return (
     <div>
@@ -34,16 +154,7 @@ export const PatientDetail = ({patient}: {patient: Patient}) => {
       <h3>Entries</h3>
       {patient.entries.map((entry) => (
         <div key={Math.floor(Math.random() * 1000000)}>
-          {entry.date} <i>{entry.description}</i>
-          <br />
-          {entry.diagnosisCodes && (
-            <ul>
-              {entry.diagnosisCodes.map((code) => {
-                const name = codes.find((d) => d.code === code);
-                return (<li key={code}>{code} {name?.name}</li>)
-              })}
-            </ul>
-          )}
+          <EntryDetails entry={entry} />
         </div>
       ))}
     </div>
