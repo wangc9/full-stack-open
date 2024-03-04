76e531d5dae6e4b9bb91a43f73364f77ccd514b1
Chen Wang <99030776+wangc9@users.noreply.github.com>
2024-03-04 13:41:40 +0200

    E 10.5 Split repositories into cards

--- part10-new/rate-repository-app/src/components/RepositoryItem.jsx
+++ part10-new/rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,6 +1,60 @@
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, Text as NativeText, View } from 'react-native';
+import theme from '../theme';
+import Text from './Text';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: theme.fontSizes.body,
+    marginTop: 0.25 * theme.fontSizes.body,
+    display: 'flex',
+  },
+  main: {
+    paddingBottom: 0.5 * theme.fontSizes.body,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  img: {
+    width: 40,
+    height: 40,
+    borderRadius: 5,
+  },
+  secondary: {
+    display: 'flex',
+    paddingLeft: 10,
+  },
+  tag: {
+    backgroundColor: theme.colors.primary,
+    color: theme.colors.navButton,
+    paddingHorizontal: 6,
+    paddingVertical: 0.35 * theme.fontSizes.body,
+    alignSelf: 'flex-start',
+    borderRadius: 5,
+  },
+  dataContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 2 * theme.fontSizes.subheading,
+    paddingTop: theme.fontSizes.subheading,
+    width: '100%',
+  },
+  data: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
 
 export default function RepositoryItem(props) {
+  const calculator = (number) => {
+    if (number < 1000) {
+      return number;
+    }
+    const result = number / 1000;
+
+    return `${result.toFixed(1)}k`;
+  };
+
   const {
     fullName,
     description,
@@ -9,16 +63,82 @@ export default function RepositoryItem(props) {
     stargazersCount,
     ratingAverage,
     reviewCount,
+    ownerAvatarUrl,
   } = props.props;
+
   return (
-    <View>
-      <Text>Full name: {fullName}</Text>
-      <Text>Description: {description}</Text>
-      <Text>Language: {language}</Text>
-      <Text>Stars: {stargazersCount}</Text>
-      <Text>Forks: {forksCount}</Text>
-      <Text>Reviews: {reviewCount}</Text>
-      <Text>Rating: {ratingAverage}</Text>
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Image style={styles.img} source={{ uri: ownerAvatarUrl }} />
+        <View style={styles.secondary}>
+          <Text
+            color="primary"
+            fontSize="subheading"
+            fontWeight="bold"
+            style={{ paddingVertical: 2 }}
+          >
+            {fullName}
+          </Text>
+          <Text
+            color="textSecondary"
+            fontSize="body"
+            style={{ paddingTop: 2, paddingBottom: 6 }}
+          >
+            {description}
+          </Text>
+          <NativeText style={styles.tag}>{language}</NativeText>
+        </View>
+      </View>
+      <View style={styles.dataContainer}>
+        <View style={styles.data}>
+          <Text
+            fontSize="subheading"
+            fontWeight="bold"
+            style={{ paddingBottom: 4 }}
+          >
+            {calculator(stargazersCount)}
+          </Text>
+          <Text color="textSecondary" style={{ paddingTop: 4 }}>
+            Stars
+          </Text>
+        </View>
+        <View style={styles.data}>
+          <Text
+            fontSize="subheading"
+            fontWeight="bold"
+            style={{ paddingBottom: 4 }}
+          >
+            {calculator(forksCount)}
+          </Text>
+          <Text color="textSecondary" style={{ paddingTop: 4 }}>
+            Forks
+          </Text>
+        </View>
+        <View style={styles.data}>
+          <Text
+            fontSize="subheading"
+            fontWeight="bold"
+            style={{ paddingBottom: 4 }}
+          >
+            {calculator(reviewCount)}
+          </Text>
+          <Text color="textSecondary" style={{ paddingTop: 4 }}>
+            Reviews
+          </Text>
+        </View>
+        <View style={styles.data}>
+          <Text
+            fontSize="subheading"
+            fontWeight="bold"
+            style={{ paddingBottom: 4 }}
+          >
+            {calculator(ratingAverage)}
+          </Text>
+          <Text color="textSecondary" style={{ paddingTop: 4 }}>
+            Rating
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
