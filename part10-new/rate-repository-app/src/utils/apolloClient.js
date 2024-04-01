de94d062872c6bde15921270c5f5685aecf2d4f1
Chen Wang <99030776+wangc9@users.noreply.github.com>
2024-04-01 11:32:18 +0300

    E 10.12 Use environment variables

--- part10-new/rate-repository-app/src/utils/apolloClient.js
+++ part10-new/rate-repository-app/src/utils/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://192.168.3.7:4000/graphql',
+    uri: Constants.expoConfig.extra.apolloURI,
     cache: new InMemoryCache(),
   });
 };
