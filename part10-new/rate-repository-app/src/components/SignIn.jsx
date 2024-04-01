cd3fb34c806db025d456d324b3abff13cf7ab598
Chen Wang <99030776+wangc9@users.noreply.github.com>
2024-04-01 09:43:08 +0300

    E 10.8 Add basic form

--- part10-new/rate-repository-app/src/components/SignIn.jsx
+++ part10-new/rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,85 @@
+import { Button, Pressable, StyleSheet, TextInput, View } from 'react-native';
 import Text from './Text';
+import { Field, Form, Formik } from 'formik';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: theme.fontSizes.body,
+    backgroundColor: theme.colors.navButton,
+  },
+  text: {
+    borderColor: theme.colors.textSecondary,
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingLeft: theme.fontSizes.body,
+    paddingVertical: theme.fontSizes.body,
+    marginBottom: theme.fontSizes.body,
+    fontSize: theme.fontSizes.subheading,
+  },
+  button: {
+    paddingVertical: theme.fontSizes.body,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 4,
+  },
+  buttonText: {
+    alignSelf: 'center',
+    fontSize: theme.fontSizes.subheading,
+    color: theme.colors.navButton,
+    fontWeight: theme.fontWeights.bold,
+  },
+});
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Formik
+        initialValues={{
+          Username: '',
+          Password: '',
+        }}
+        onSubmit={onSubmit}
+        style={styles.container}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View>
+            <TextInput
+              onChangeText={handleChange('Username')}
+              onBlur={handleBlur('Username')}
+              value={values.Username}
+              placeholder="Username"
+              style={styles.text}
+            />
+            <TextInput
+              onChangeText={handleChange('Password')}
+              onBlur={handleBlur('Password')}
+              value={values.Password}
+              placeholder="Password"
+              style={styles.text}
+              secureTextEntry
+            />
+            <Pressable
+              style={({ pressed }) =>
+                pressed
+                  ? {
+                      ...styles.button,
+                      backgroundColor: theme.colors.pressedButton,
+                    }
+                  : styles.button
+              }
+              onPress={handleSubmit}
+            >
+              <Text style={styles.buttonText}>Sign in</Text>
+            </Pressable>
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
 };
 
 export default SignIn;
