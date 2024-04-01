6ba9cc7aab03a7a6dd9e341dbb5b8f9ca9777004
Chen Wang <99030776+wangc9@users.noreply.github.com>
2024-04-01 12:42:18 +0300

    E 10.14 Store access token

--- part10-new/rate-repository-app/src/hooks/useSignIn.js
+++ part10-new/rate-repository-app/src/hooks/useSignIn.js
@@ -1,13 +1,17 @@
 import { useMutation } from '@apollo/client';
 import { AUTHENTICATE } from '../graphql/mutations';
+import AuthStorage from '../utils/authStorage';
 
 export default function useSignIn() {
+  const tokenStorage = new AuthStorage();
   const [mutate, result] = useMutation(AUTHENTICATE);
 
   const signIn = async ({ username, password }) => {
     const { data, ...response } = await mutate({
       variables: { credentials: { username, password } },
     });
+
+    await tokenStorage.setAccessToken(data.authenticate.accessToken);
 
     return { authenticate: data.authenticate, ...response };
   };
